39b04fe688ef45ac90014c8ea3be58738812eccb
Mạnh Đạt <dat.tm24@gmail.com>
2023-04-03 08:52:33 +0700

    add missing modules

---
+++ day-2/age-input.py
@@ -0,0 +1,6 @@
+age = int(input("Hi there, please enter your age: "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are not yet an adult.")
---
+++ day-2/functions.py
@@ -0,0 +1,20 @@
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+def greet_general():
+    print("Hello, hi")
+
+
+greet("Dat")
+greet_general()
+
+
+def square(x):
+    return x * x
+
+
+result = square(5)
+print(result)
+
+
